ef035fb262c87979db9cc87e452c5d6edb4cf3d6
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-01 18:32:24 -0600

    Agregar archivos de proyecto.

---
+++ mayor que/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace mayor_que
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trcnumero1 = new System.Windows.Forms.TrackBar();
+            this.trcnumero2 = new System.Windows.Forms.TrackBar();
+            this.lblmostrar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcnumero1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcnumero2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(63, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trcnumero1
+            // 
+            this.trcnumero1.Location = new System.Drawing.Point(12, 30);
+            this.trcnumero1.Name = "trcnumero1";
+            this.trcnumero1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trcnumero1.Size = new System.Drawing.Size(45, 350);
+            this.trcnumero1.TabIndex = 1;
+            this.trcnumero1.Scroll += new System.EventHandler(this.trcnumero1_Scroll);
+            // 
+            // trcnumero2
+            // 
+            this.trcnumero2.Location = new System.Drawing.Point(884, 30);
+            this.trcnumero2.Name = "trcnumero2";
+            this.trcnumero2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trcnumero2.Size = new System.Drawing.Size(45, 350);
+            this.trcnumero2.TabIndex = 2;
+            this.trcnumero2.Scroll += new System.EventHandler(this.trcnumero2_Scroll);
+            // 
+            // lblmostrar
+            // 
+            this.lblmostrar.AutoSize = true;
+            this.lblmostrar.Location = new System.Drawing.Point(480, 417);
+            this.lblmostrar.Name = "lblmostrar";
+            this.lblmostrar.Size = new System.Drawing.Size(16, 13);
+            this.lblmostrar.TabIndex = 3;
+            this.lblmostrar.Text = "   ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 548);
+            this.Controls.Add(this.lblmostrar);
+            this.Controls.Add(this.trcnumero2);
+            this.Controls.Add(this.trcnumero1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcnumero1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trcnumero2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trcnumero1;
+        private System.Windows.Forms.TrackBar trcnumero2;
+        private System.Windows.Forms.Label lblmostrar;
+    }
+}
+
---
+++ mayor que/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayor_que
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trcnumero1_Scroll(object sender, EventArgs e)
+        {
+            MayorQue();
+        }
+
+        private void trcnumero2_Scroll(object sender, EventArgs e)
+        {
+            MayorQue();
+        }
+
+        public void MayorQue()
+        {
+            Graphics papel = pictureBox1.CreateGraphics();
+            SolidBrush brocha1 = new SolidBrush(Color.Red);
+            SolidBrush brocha2 = new SolidBrush(Color.Blue);
+            int valor1, valor2;
+
+            //track 1 lado izquierdo
+
+            trcnumero1.Maximum = pictureBox1.Height;
+
+            //track 1 lado izquierdo
+            trcnumero2.Maximum = pictureBox1.Height;
+            valor1 = trcnumero1.Value;
+            valor2 = trcnumero2.Value;
+            papel.Clear(Color.White);
+
+            papel.FillEllipse(brocha1, 10, 10, valor1, valor1);
+
+            papel.FillEllipse(brocha2, 400,10, valor2, valor2);
+
+
+
+
+            if (valor2 > valor1)
+            {
+                lblmostrar.Text = "El circulo AZUL es mas Grande ";
+                
+            }
+            else
+            {
+                lblmostrar.Text = "El circulo ROJO es mas Grande;";
+
+            }
+        }
+
+
+    }
+}
